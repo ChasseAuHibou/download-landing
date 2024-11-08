1a9ea075d6f40fe7ec508cca2fa5f465b9bfa621
Dmytro Kharytonchuk <kharitonchuk.dima@gmail.com>
2024-11-08 14:35:30 +0200

    v08.11

--- src/components/Download.js
+++ src/components/Download.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./Button.css";
 
 const Download = () => {
-	const fileId = "1laNUq-18YUJFQvXvyQgQZrV8c9rDiVLV";
+	const fileId = "1eLpAi6UgEGxPlGr50fnnaS-v1iUqbD7O";
 	const fileUrl = `https://drive.google.com/uc?export=download&id=${fileId}`;
 
 	return (
 		<a href={fileUrl} download className="button">
-			Download file
+			Download Presentation
 		</a>
 	);
 };
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,8 +6,29 @@ import Download from "./Download";
 const Hero = () => {
 	return (
 		<div className="hero">
-			<div className="hero-content">
+			{/* Элемент сверху */}
+			<div className="hero-item top">
+				<h2>Hellmann East Europe Overseas LLC</h2>
+			</div>
+			
+			{/* Центральный элемент */}
+			<div className="hero-item center">
 				<Download />
+			</div>
+
+			{/* Элемент снизу */}
+			<div className="hero-item bottom">
+				<p>
+					Waiting for Your request:
+					<br />
+					<br />
+					Dariia Iarmolovych - diarmolo@hellmann.com
+					<br /> +3 80 50 490 34 30 / +3 80 63 136 2278
+					<br />
+					<br />
+					Yana Lystopad - ylystopa@hellmann.com
+					<br /> +38 068 825 96 33
+				</p>
 			</div>
 		</div>
 	);
